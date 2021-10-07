d25067f87c7d37be86f796de84c6a3d90cb175c0
alapvi <alapvi@yahoo.com>
2021-10-07 08:01:14 +0200

    DNI

--- dni.py
+++ dni.py
@@ -1,3 +1,18 @@
-dni = input("Your DNI:")
-for i in dni:
-    print(i)
+letters = "TRWAGMYFPDXBNJZSQVHLCKE"
+
+while True:
+    dni = input("Your DNI:")
+    letter = dni[-1].upper()
+    number = dni[:-1]
+    if (len(dni) == 9 and number.isdigit()):
+        number = int(dni[:-1])
+        break
+    else:
+        print("Format Incorect")
+
+mod = number%23
+if letters[mod] == letter:
+    print("Correct")
+else:
+    print("Incorrect!")
+
